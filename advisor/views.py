4fa839acc3feefd28e922badac1460ed9d037319
thisisthechris <mail@thisisthechris.co.uk>
2013-03-12 11:50:58 +0000

    Adds first page function

--- advisor/views.py
+++ advisor/views.py
@@ -1,1 +1,15 @@
 # Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
+from django.contrib.auth.decorators import login_required
+from django.template import Context, loader, RequestContext
+from django.template.loader import render_to_string
+from django.contrib.auth.models import User
+from django.conf import settings
+from django.shortcuts import render_to_response, get_object_or_404, redirect
+from django.core.mail import EmailMessage
+
+import datetime, math
+
+def page(request):
+
+    return render_to_response('page.html', {}, context_instance=RequestContext(request))
